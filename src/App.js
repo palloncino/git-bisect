48785ef271d152167484ee2e9cc99eb59d79dd92
Antonio Guiotto <guiotto@Antonios-MacBook-Pro.local>
2022-05-06 12:39:47 +0200

    commit 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>My new app!</h2>
+        <h2>My new rebranded app!</h2>
         <p>
           This is commit n. 2
         </p>
